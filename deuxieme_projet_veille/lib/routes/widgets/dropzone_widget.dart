19fe2472c31807505a1ec570d60aa306e6d503f2
ErwanDerrien <erwan1derrien@gmail.com>
2021-11-23 12:41:57 -0500

    implemented fix for IOS situation

--- deuxieme_projet_veille/lib/routes/widgets/dropzone_widget.dart
+++ deuxieme_projet_veille/lib/routes/widgets/dropzone_widget.dart
@@ -1,10 +1,11 @@
 // import 'package:deuxieme_projet_veille/routes/widgets/dropzone_widget.dart';
 // ignore_for_file: unnecessary_new
 
-import 'dart:html';
+// import 'dart:html';
 import 'dart:typed_data';
 
 import 'package:deuxieme_projet_veille/routes/file_upload.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dropzone/flutter_dropzone.dart';
 import 'package:http/http.dart' as http;
@@ -17,9 +18,10 @@ import 'package:http_parser/http_parser.dart';
 
 import 'package:flutter/material.dart';
 // import 'package:image_picker/image_picker.dart';
-// import 'dart:io';
 import 'package:path/path.dart';
 import 'package:dio/dio.dart';
+
+import 'dart:io' show Platform;
 
 class DropzoneWidget extends StatefulWidget {
   const DropzoneWidget({Key? key, required this.token}) : super(key: key);
@@ -39,6 +41,69 @@ class _DropzoneWidgetState extends State<DropzoneWidget> {
 
   @override
   Widget build(BuildContext context) {
+    if ((defaultTargetPlatform == TargetPlatform.iOS) ||
+        (defaultTargetPlatform == TargetPlatform.android)) {
+      return Container(
+        color: Colors.green,
+        child: Stack(children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.cloud_upload,
+                  size: 80,
+                  color: Colors.white,
+                ),
+                Text('Implementation mobile à venir...',
+                    style: TextStyle(color: Colors.white, fontSize: 24)),
+                SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ]),
+      );
+    } else if ((defaultTargetPlatform == TargetPlatform.linux) ||
+        (defaultTargetPlatform == TargetPlatform.macOS) ||
+        (defaultTargetPlatform == TargetPlatform.windows)) {
+      return Container(
+        color: Colors.green,
+        child: Stack(children: [
+          DropzoneView(
+              onCreated: (controller) => this.controller = controller,
+              onDrop: acceptFile),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.cloud_upload,
+                  size: 80,
+                  color: Colors.white,
+                ),
+                const Text('Déposez votre CV',
+                    style: TextStyle(color: Colors.white, fontSize: 24)),
+                const SizedBox(height: 16),
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    // ignore: unused_local_variable
+                    final events = await controller.pickFiles();
+                    if (events.isEmpty) return;
+
+                    acceptFile(events.first);
+                  },
+                  icon: const Icon(Icons.search, size: 32),
+                  label: const Text('Sélectionnez le fichier',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      );
+    } else {
+      print('// Some web specific code there');
+    }
     return Container(
       color: Colors.green,
       child: Stack(children: [
